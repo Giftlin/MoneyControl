ca908a61eaa8de4762008bc77aa059e144b6d8f9
Giftlin Rajaiah <giftlin.rgn@gmail.com>
2021-07-15 09:32:49 +0530

    Update step_1.py

--- src/step_1.py
+++ src/step_1.py
@@ -17,6 +17,31 @@ for element in list_page:
     URL = "https://www.moneycontrol.com/india/stockpricequote/" + element
     driver.get(URL)
     html = html + "        " + driver.page_source
+    
+lst = html.split("</a>")
+lst = [x.split('href="')[1]
+       for x in lst if "https://www.moneycontrol.com/india/stockpricequote/" in x]
+lst_url = [x.split('"')[0] for x in lst]
+lst_name = [x.split(">")[-1] if len(x.split('" title="')) < 2 else x.split(
+    '" title="')[1].split('"')[0] for x in lst]
+print(len(lst))
+print(lst)
+print(lst_name)
 
-with open("files/html_stock.txt", "a", encoding="utf-8") as f:
-    f.write(html)
+
+list_page = ["","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", '<link rel="stylesheet" ',
+             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "OTHERS", "Others", '<link rel="stylesheet"', "", "Login", """
+ < link """, """
+ <link """]
+
+df = pd.DataFrame({"NAME": lst_name, "URL": lst_url})
+df = df.drop_duplicates("URL")
+df = df[~df.NAME.isin(list_page)]
+print(df)
+df["PRICE"] = ""
+df["STRENGTHS"] = ""
+df["WEAKNESSES"] = ""
+df["OPPORTUNITIES"] = ""
+df["THREATS"] = ""
+df["COMPLETED"] = "NO"
+df.to_csv("stock_urls.csv", index=False)
