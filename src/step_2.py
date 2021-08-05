ba309ac69b683b6574754287979ab28f70de0cfb
Giftlin Rajaiah <giftlin.rgn@gmail.com>
2021-08-05 22:18:39 +0530

    Update step_2.py

--- src/step_2.py
+++ src/step_2.py
@@ -6,22 +6,31 @@ options.add_argument("start-maximized")
 options.add_argument("--headless")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 df = pd.read_csv("files/stock_urls.csv")
-new_df = df[df.COMPLETED=="NO"]
+new_df = df[df.COMPLETED == "NO"]
 new_urls = new_df.URL
 for URL in new_urls:
     driver.get(URL)
     html = driver.page_source
     try:
-        val = html.split('data-numberanimate-value="')[1].split('"')[0]
+        try:
+            val = html.split('data-numberanimate-value="')[1].split('"')[0]
+        except Exception:
+            try:
+                val = html.split('<input id="bsespotval" type="hidden" value="')[
+                    1].split('"')[0]
+            except Exception:
+                val = ""
+        try:
+            s = html.split("<strong>Strengths (")[1].split(")")[0]
+            w = html.split("<strong>Weaknesses (")[1].split(")")[0]
+            o = html.split("<strong>Opportunities (")[1].split(")")[0]
+            t = html.split("<strong>Threats (")[1].split(")")[0]
+        except Exception:
+            s, w, o, t = "", "", "", ""
         df.loc[df.URL == URL, 'PRICE'] = val
-        print(val)
-        s = html.split("<strong>Strengths (")[1].split(")")[0]
         df.loc[df.URL == URL, 'STRENGTHS'] = s
-        w = html.split("<strong>Weaknesses (")[1].split(")")[0]
         df.loc[df.URL == URL, 'WEAKNESSES'] = w
-        o = html.split("<strong>Opportunities (")[1].split(")")[0]
         df.loc[df.URL == URL, 'OPPORTUNITIES'] = o
-        t = html.split("<strong>Threats (")[1].split(")")[0]
         df.loc[df.URL == URL, 'THREATS'] = t
     except Exception:
         pass
